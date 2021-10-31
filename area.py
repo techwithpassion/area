478c87b744e1bc31fda546570a0c43c107c9481b
rahulsinghrdj <75153249+rahulsinghrdj@users.noreply.github.com>
2021-10-31 19:12:20 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,6 @@
 ch='y'
 while ch=='y' or ch=='Y':
-    x=input("1.area of rectangle\n 2.area of circle\n 3.area of square\n")
+    x=input("1.area of rectangle\n 2.area of circle\n 3.area of square\n 4. area of triangle\n")
     if(x=='1'):
         l=int(input("Enter the lenght of rectangle="))
         b=int(input("Enter the breadth of rectangle="))
@@ -11,6 +11,12 @@ while ch=='y' or ch=='Y':
     elif(x=='3'):
         s=int(input("Enter side of the square="))
         print("AREA OF SQUARE=",s**2)
+    elif(x=='4'):
+        b = int(input("enter the base of triangle = "))
+        h = int(input("enter the height of triangle = "))
+        print("area of triangle is" , 1/2 * b * h)
+    else:
+        print("wrong input")
     ch=input("do you want more calculations y\n")
 print("Thanks for using!!")
              
